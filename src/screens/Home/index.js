9637430ace158eaa22753032fe93729ec45deb5c
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-06-13 08:45:31 -0400

    Added an mtg sdk, to play around with different apis

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = ({}) => {
-  return <h1>Home Page</h1>;
+  const [name, setName] = useState("");
+  const [cardImage, setCardImage] = useState("");
+  const mtg = require("mtgsdk");
+  const handleSubmit = () => {
+    console.log("Submitted", name);
+    mtg.card
+      .all({
+        name: name
+      })
+      .on("data", card => {
+        setCardImage(card);
+        console.log(card);
+      });
+  };
+  return (
+    <>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input
+          onChange={e => setName(e.target.value)}
+          type="text"
+          value={name}
+        />
+        <input type="button" value="submit" />
+      </form>
+      {cardImage.name}
+      <img src={cardImage.imageUrl} alt="" />
+    </>
+  );
 };
 
 export default Home;
