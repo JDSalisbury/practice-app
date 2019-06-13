221043bc07407c7aa7f7d0a6733862ceb0035c27
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-06-12 21:02:44 -0400

    Redux set up

---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+export default class Config {
+  static baseUrl = (function() {
+    let baseUrl;
+
+    if (process.env.NODE_ENV === "development") {
+      baseUrl = "http://localhost:8000/";
+    } else {
+      baseUrl = "http://ec2-13-59-76-206.us-east-2.compute.amazonaws.com:8000/";
+    }
+    return baseUrl;
+  })();
+
+  static serviceHeaders = {
+    "Content-Type": "application/json"
+  };
+
+  // static setAuthToken(token) {
+  //   this.serviceHeaders.Authorization = "Token " + token;
+  // }
+}
